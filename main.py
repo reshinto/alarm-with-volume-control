a080dc3ed0842bab058c3207e0b8370baa3c1b53
Terence <35042213+reshinto@users.noreply.github.com>
2018-04-06 03:06:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 from schalarm import *
 
 def combi_sch(combi):
-	"""
-	Initialize alarm time.
-	Set the basic settings to allow combinations of user input for alarm schedule.
-	"""
-	al_hr = input(" Please set the alarm hour (xx) time: ")
-	al_min = input(" Please set the alarm minute (xx) time: ")
-	al_time = al_hr + ":" + al_min
-	# Looking for ways to simplify and make the following code more efficient
-	a = schedule.every().monday.at(al_time).do(job)
-	b = schedule.every().tuesday.at(al_time).do(job)
-	c = schedule.every().wednesday.at(al_time).do(job)
-	d = schedule.every().thursday.at(al_time).do(job)
-	e = schedule.every().friday.at(al_time).do(job)
-	f = schedule.every().saturday.at(al_time).do(job)
-	g = schedule.every().sunday.at(al_time).do(job)
-	run_schedule(combinations(combi, 7))
+    """
+    Initialize alarm time.
+    Set the basic settings to allow combinations of user input for alarm schedule.
+    """
+    al_hr = input(" Please set the alarm hour (xx) time: ")
+    al_min = input(" Please set the alarm minute (xx) time: ")
+    al_time = al_hr + ":" + al_min
+    # Looking for ways to simplify and make the following code more efficient
+    a = schedule.every().monday.at(al_time).do(job)
+    b = schedule.every().tuesday.at(al_time).do(job)
+    c = schedule.every().wednesday.at(al_time).do(job)
+    d = schedule.every().thursday.at(al_time).do(job)
+    e = schedule.every().friday.at(al_time).do(job)
+    f = schedule.every().saturday.at(al_time).do(job)
+    g = schedule.every().sunday.at(al_time).do(job)
+    run_schedule(combinations(combi, 7))
 
 def main():
-	"""
-	The heart of the program, and displays alarm menu.
-	Links settings to alarm schedules.
-	"""
-	combi = input("""\
+    """
+    The heart of the program, and displays alarm menu.
+    Links settings to alarm schedules.
+    """
+    combi = input("""\
  Choose a combination of the following:
  Weekdays   : abcde
  Weekends   : fg 
@@ -35,14 +35,14 @@ def main():
  Saturdays  : f
  Sundays    : g
  > """)
-	# Needs work! prevent user from selecting invalid values
-	if combi != "h":
-		combi_sch(combi)
-	else:
-		print("Wrong")
-		main()
+    # Needs work! prevent user from selecting invalid values
+    if combi != "h":
+        combi_sch(combi)
+    else:
+        print("Wrong")
+        main()
 
 # time_input() function is activated first to input required settings
 if __name__=="__main__":
-	time_input()
-	main()
+    time_input()
+    main()
